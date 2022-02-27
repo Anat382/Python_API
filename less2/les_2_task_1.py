3d723b45afc515fb4cf6b997d6ed9377f5acdae4
Anat382 <62346931+Anat382@users.noreply.github.com>
2022-02-27 13:48:26 +0700

    Update les_2_task_1.py
    change code

--- less2/les_2_task_1.py
+++ less2/les_2_task_1.py
@@ -22,78 +22,90 @@ import re
 import pretty_html_table
 import os
 import webbrowser
+from time import perf_counter
 
 
-def connect_url(job, count_page=0):
+def connect_url(job):
 
     # headers = {r'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
     headers = {'User-Agent': 'Mozilla/5.0 (Widnows NT 5.1; rv:47.0) Gecko/20100101 FireFox/47.0',
                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
     base_url = r'https://hh.ru/'
-    url = f'{base_url}search/vacancy?text={job}&page={str(count_page)}'
-    print(url)
-    data_job = []
-    try:
-        response = requests.get(url, headers=headers)
-        # print(response.status_code)
-        if response.status_code == 404:
-            raise BaseException(f'Ошибка подключения 404')
-        dom = BeautifulSoup(response.text, 'html.parser')
-        # pprint(dom)
-        articles = dom.find_all('div', {'class': 'vacancy-serp-item'})
-        # print(articles)
-        for art in articles:
-            data_json = {}
+    count_n = 0
+    while True:
+        url = f'{base_url}search/vacancy?text={job}&page={count_n}'
+        numer = requests.get(url, headers=headers)
+        print(count_n, numer.status_code)
+        if numer.status_code == 404:
+            break
+        # elif count_n == 3:
+        #     break
+        count_n += 1
+    print(count_n)
 
-            job_url = art.find('a', {'class': 'bloko-link'})
-            salary = art.find('div', {'class': 'bloko-v-spacing bloko-v-spacing_base-3'})
-            company_url = art.find('a', {'class': 'bloko-link bloko-link_kind-tertiary'})
-            data_ = art.find('span', {'class': 'bloko-text'})
-            local = art.find('div', {'class': 'bloko-text bloko-text_no-top-indent'})
+    df = pd.DataFrame()
+    for num in range(count_n):
+        url = f'{base_url}search/vacancy?text={job}&page={num}'
+        print(url)
+        data_job = []
+        try:
+            response = requests.get(url, headers=headers)
+            dom = BeautifulSoup(response.text, 'html.parser')
+            # pprint(dom)
+            articles = dom.find_all('div', {'class': 'vacancy-serp-item'})
+            # print(articles)
+            for n, art in enumerate(articles):
+                data_json = {}
 
-            url_job_ = job_url['href'] if job_url else None
-            job_name = job_url.getText() if job_url else None
-            get_salary = salary.previousSibling.getText()  if salary.previousSibling else None
-            company_name = company_url.getText() if company_url else None
-            company_url = company_url['href'] if company_url else None
-            date_send = data_.getText() if data_ else None
-            place_work = local.getText() if local else None
+                job_url = art.find('a', {'class': 'bloko-link'})
+                salary = art.find('div', {'class': 'bloko-v-spacing bloko-v-spacing_base-3'})
+                company_url = art.find('a', {'class': 'bloko-link bloko-link_kind-tertiary'})
+                data_ = art.find('span', {'class': 'bloko-text'})
+                local = art.find('div', {'class': 'bloko-text bloko-text_no-top-indent'})
 
-            data_json['page'] = count_page
-            data_json['url_web'] = base_url
-            data_json['url_job_'] = url_job_
-            data_json['job_name'] = job_name
-            data_json['get_salary'] = get_salary
-            data_json['company_name'] = company_name
-            data_json['company_url'] = base_url + company_url if company_url else None
-            data_json['date_send'] = date_send
-            data_json['place_work'] = place_work
-            data_job.append(data_json)
+                url_job_ = job_url['href'] if job_url else None
+                job_name = job_url.getText() if job_url else None
+                get_salary = str(salary.previousSibling.getText()).replace('\u202f', '') if salary.previousSibling else None
+                company_name = company_url.getText() if company_url else None
+                company_url = company_url['href'] if company_url else None
+                date_send = data_.getText() if data_ else None
+                place_work = local.getText() if local else None
 
-            # print(url_job_, job_name, str(get_salary).encode('utf-8').decode('utf-8'), company_name, company_url, date_send, place_work)
-            # print(job_url['href'], job_url.getText(), salary.previousSibling.getText(), company_url.getText(), company_url['href']) # company_url['href']
+                salary_num = re.findall(r'\d+', get_salary)
+                world_simb = re.compile(r'[а-я]\w+|[А-Я]\w+')
+                world = re.findall(world_simb, get_salary)
+                currency = world[-1] if world else None
+                befor = 'от' if 'от' in world else None
+                after = 'до' if 'до' in world else None
+                data_json['page'] = num
+                data_json['url_web'] = base_url
+                data_json['url_job_'] = url_job_
+                data_json['job_name'] = job_name
+                data_json['min_salary'] = int(salary_num[0]) if len(salary_num) == 2 or (salary_num and befor) else 0
+                data_json['max_salary'] = int(salary_num[-1]) if len(salary_num) == 2 or (salary_num and after) else 0
+                data_json['currency'] = currency
+                data_json['company_name'] = str(company_name).replace('\xa0', ' ') #  'ООО\xa0ХеленФуд',
+                data_json['company_url'] = base_url + company_url if company_url else None
+                data_json['date_send'] = date_send
+                data_json['place_work'] = str(place_work).replace('\xa02\xa0', '').replace('\xa01\xa0', '').replace('\xa03\xa0', '')  #'Москва, Борисово и еще\xa02\xa0'
 
-            # print(data_json)
-        df = pd.json_normalize(data_job)
-        return df
+                data_job.append(data_json)
 
-    except Exception as exp:
-        print(exp, '\n', response.status_code)
+                # print(url_job_, job_name, str(get_salary).encode('utf-8').decode('utf-8'), company_name, company_url, date_send, place_work)
+                # print(job_url['href'], job_url.getText(), salary.previousSibling.getText(), company_url.getText(), company_url['href']) # company_url['href']
+            df = df.append(pd.json_normalize(data_job))
+        except Exception as exp:
+            print(exp, '\n', exp.args)
+    return df
+
 
 # Программист
 job = str(input('Введите профессию, должность, компанию: ').encode('UTF-8')).replace(r'\x','%').upper()[2:-1]
+start = perf_counter()
+df = connect_url(job)
+print(perf_counter() - start)
 
-
-df = pd.DataFrame()
-count_n = 0
-while True:
-    if count_n == 5:
-        break
-    else:
-        df = df.append(connect_url(job, count_n))
-
-    count_n += 1
-# df.to_html(encodin='utf-8')
+a = 1
 html_res = pretty_html_table.build_table(df, 'blue_light')
 
 
@@ -106,4 +118,3 @@ path = 'temp.html'
 with open(path, 'w', encoding='utf-8') as f:
     f.write(str(html_res))
 webbrowser.open(path)
-
